f5c778f0b19fcefff160dd0b6d1dd3bde867534b
Hajredin Abidu <diniabdiu@gmail.com>
2019-04-29 12:14:55 +0200

    init

---
+++ app.js
@@ -0,0 +1,39 @@
+// require('dotenv').config();
+var bodyParser = require('body-parser');
+mongoose       = require('mongoose'),
+express        = require('express'),
+app            = express();
+// const PORT = parseInt(process.env.PORT);
+// App config
+mongoose.connect('mongodb://localhost/restful_blog_app', {useNewUrlParser: true});
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Mongoose/model Config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model('Blog', blogSchema);
+// RESTFUL ROUTES
+app.get('/', function(req, res) {
+    res.redirect('/blogs')
+});
+app.get('/blogs', function(req, res) {
+    Blog.find({}, function(err, blogs) {
+        if(err){
+            console.log('ERROR!');
+        } else {
+             res.render('index', {blogs});
+        }
+    });
+    res.render('index');
+});
+
+app.listen(3000, process.env.IP, function(req, res) {
+
+    console.log('test');
+});
